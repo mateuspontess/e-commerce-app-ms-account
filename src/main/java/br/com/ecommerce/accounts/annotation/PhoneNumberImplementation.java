51a7a0b934d837a2e64841fc0c9d7f58d59a2b92
Mateus Pontes <mateuspsdd@gmail.com>
2024-05-13 17:12:47 -0300

    Update PhoneNumberImplementation.class

--- src/main/java/br/com/ecommerce/accounts/annotation/PhoneNumberImplementation.java
+++ src/main/java/br/com/ecommerce/accounts/annotation/PhoneNumberImplementation.java
@@ -1,3 +1,4 @@
+
 package br.com.ecommerce.accounts.annotation;
 
 import com.google.i18n.phonenumbers.NumberParseException;
@@ -14,16 +15,19 @@ public class PhoneNumberImplementation implements ConstraintValidator<PhoneNumbe
 	
 	@Override
 	public boolean isValid(String phoneNumber, ConstraintValidatorContext context) {
-		PhoneNumberUtil pnUtil = PhoneNumberUtil.getInstance();
-		PhoneNumber pn;
-		
-		try {
-			pn = pnUtil.parse(phoneNumber, "BR");
+		if (phoneNumber != null && !phoneNumber.isEmpty()) {
+			PhoneNumberUtil pnUtil = PhoneNumberUtil.getInstance();
+			PhoneNumber pn;
 			
-		} catch (NumberParseException e) {
-			return false;
-		}
-		
-		return pnUtil.isValidNumberForRegion(pn, "BR");
+			try {
+				pn = pnUtil.parse(phoneNumber, "BR");
+				
+			} catch (NumberParseException e) {
+				return false;
+			}
+			
+			return pnUtil.isValidNumberForRegion(pn, "BR");
+		}	
+		return false;
 	}
 }
