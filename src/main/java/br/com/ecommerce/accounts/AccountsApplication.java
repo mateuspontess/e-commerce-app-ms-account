f61f6583e6006ce85e0f6bac76904cd8a522e43f
Mateus Pontes <mateuspsdd@gmail.com>
2024-05-15 12:39:54 -0300

    ...

--- src/main/java/br/com/ecommerce/accounts/AccountsApplication.java
+++ src/main/java/br/com/ecommerce/accounts/AccountsApplication.java
@@ -1,12 +1,24 @@
 package br.com.ecommerce.accounts;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class AccountsApplication {
+import br.com.ecommerce.accounts.configs.UserAdminCreation;
 
+@SpringBootApplication
+public class AccountsApplication implements CommandLineRunner {
+
+	@Autowired
+	UserAdminCreation uac;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AccountsApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		uac.createUserAdmin();
 	}
 }
