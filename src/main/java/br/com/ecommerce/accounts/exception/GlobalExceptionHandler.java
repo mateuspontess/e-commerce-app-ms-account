3863f7cfcb2f9105cbeb7b0e5dd85a7f24af76ac
Mateus Pontes <mateuspsdd@gmail.com>
2024-06-09 08:35:20 -0300

    complete refactoring of `GlobalExceptionHandler`

--- src/main/java/br/com/ecommerce/accounts/exception/GlobalExceptionHandler.java
+++ src/main/java/br/com/ecommerce/accounts/exception/GlobalExceptionHandler.java
@@ -20,6 +20,56 @@ public class GlobalExceptionHandler {
 	private final String ENTITY_NOT_FOUND_EXCEPTION = "User not found";
 	private final String METHOD_ARGUMENT_NOT_VALID_MESSAGE = "Input validation error";
 	private final String INTERNAL_SERVER_ERROR_MESSAGE = "Internal server error";
+	
+	
+	@ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<?> handleError404(NoResourceFoundException ex) {
+    	return ResponseEntity.notFound().build();
+    }
+
+	@ExceptionHandler(EntityNotFoundException.class)
+	public ResponseEntity<ErrorMessage> handleError401(EntityNotFoundException ex) {
+		return ResponseEntity
+				.status(HttpStatus.UNAUTHORIZED)
+				.body(new ErrorMessage(
+						HttpStatus.UNAUTHORIZED.value(), 
+						ENTITY_NOT_FOUND_EXCEPTION
+						)
+					);
+	}
+	
+	@ExceptionHandler(FailedCredentialsException.class)
+	public ResponseEntity<ErrorMessage> handleError401(FailedCredentialsException ex) {
+		return ResponseEntity
+				.status(HttpStatus.UNAUTHORIZED)
+				.body(new ErrorMessage(
+					HttpStatus.UNAUTHORIZED.value(), 
+					CREDENTIALS_ERROR_MESSAGE));
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorMessageWithFields> handleError400(MethodArgumentNotValidException ex) {
+    	var fields = ex.getFieldErrors().stream().collect(Collectors.toMap(f -> f.getField().toString(), f -> f.getDefaultMessage()));
+    	
+    	return ResponseEntity
+    			.badRequest()
+    			.body(new ErrorMessageWithFields(
+					HttpStatus.BAD_REQUEST.value(),
+					METHOD_ARGUMENT_NOT_VALID_MESSAGE,
+					fields
+					)
+				);
+    }
+	
+	@ExceptionHandler(IllegalArgumentException.class)
+	public ResponseEntity<ErrorMessage> handlerErro400(IllegalArgumentException ex) {
+		return ResponseEntity.badRequest().body(new ErrorMessage(HttpStatus.BAD_REQUEST.value(), ex.getMessage()));
+	}
+
+	@ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<String> handleError400(HttpMessageNotReadableException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
 
 	@ExceptionHandler(HttpMediaTypeNotSupportedException.class)
     public ResponseEntity<ErrorMessage> handlerErro415(HttpMediaTypeNotSupportedException ex) {
@@ -31,60 +81,18 @@ public class GlobalExceptionHandler {
 
         return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE.value()).body(new ErrorMessage(HttpStatus.UNSUPPORTED_MEDIA_TYPE.value(), message));
     }
-	
-	@ExceptionHandler(HttpMessageNotReadableException.class)
-	public ResponseEntity<ErrorMessage> handleError400(HttpMessageNotReadableException ex) {
-		return ResponseEntity.badRequest().body(new ErrorMessage(HttpStatus.BAD_REQUEST.value(), ex.getMessage().split(":")[0]));
-	}
-	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ResponseEntity<ErrorMessageWithFields> handlerError400(MethodArgumentNotValidException ex) {
-		var fields = ex.getFieldErrors().stream()
-				.map(f -> new FieldErrorResponse(f.getField().toString(), f.getDefaultMessage()));
-		
-		return ResponseEntity
-				.badRequest()
-				.body(new ErrorMessageWithFields(
-					HttpStatus.BAD_REQUEST.value(),
-					METHOD_ARGUMENT_NOT_VALID_MESSAGE,
-					fields));
-	}
-	@ExceptionHandler(IllegalArgumentException.class)
-	public ResponseEntity<ErrorMessage> handlerErro400(IllegalArgumentException ex) {
-		return ResponseEntity.badRequest().body(new ErrorMessage(HttpStatus.BAD_REQUEST.value(), ex.getMessage()));
-	}
 
-	@ExceptionHandler(EntityNotFoundException.class)
-	public ResponseEntity<ErrorMessage> handleError404(EntityNotFoundException ex) {
-		return ResponseEntity
-				.status(HttpStatus.UNAUTHORIZED)
-				.body(new ErrorMessage(
-					HttpStatus.NOT_FOUND.value(), 
-					ENTITY_NOT_FOUND_EXCEPTION));
-	}
-	@ExceptionHandler(NoResourceFoundException.class)
-    public ResponseEntity<?> handleError404(NoResourceFoundException ex) {
-    	return ResponseEntity.notFound().build();
-    }
-	
-	@ExceptionHandler(FailedCredentialsException.class)
-	public ResponseEntity<ErrorMessage> handleError401(FailedCredentialsException ex) {
-		return ResponseEntity
-				.status(HttpStatus.UNAUTHORIZED)
-				.body(new ErrorMessage(
-					HttpStatus.UNAUTHORIZED.value(), 
-					CREDENTIALS_ERROR_MESSAGE));
-	}
-	
 	@ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorMessage> handlerError500(Exception ex) {
+    public ResponseEntity<ErrorMessage> handleError500(Exception ex) {
         return ResponseEntity
         		.internalServerError()
         		.body(new ErrorMessage(
         				HttpStatus.INTERNAL_SERVER_ERROR.value(),
-        				INTERNAL_SERVER_ERROR_MESSAGE));
+        				INTERNAL_SERVER_ERROR_MESSAGE
+						)
+					);
     }
 	
 	private record ErrorMessage(int status, Object error) {}
 	private record ErrorMessageWithFields(int status, String error, Object fields) {}
-	private record FieldErrorResponse(String field, String message) {}
 }
