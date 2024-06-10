b8bab7a4ed79403775f17a7bd7314b44dbc31663
Mateus Pontes <mateuspsdd@gmail.com>
2024-06-10 16:23:41 -0300

    refactor: `GlobalExceptionHandler`

--- src/main/java/br/com/ecommerce/accounts/exception/GlobalExceptionHandler.java
+++ src/main/java/br/com/ecommerce/accounts/exception/GlobalExceptionHandler.java
@@ -7,7 +7,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
