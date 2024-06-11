c963aefca7797ed86319acb597fc3a4961af6091
Mateus Pontes <mateuspsdd@gmail.com>
2024-06-10 22:10:12 -0300

    test: add integration terst `UserAdminCreationTest`

---
+++ src/test/java/br/com/ecommerce/accounts/integration/UserAdminCreationTest.java
@@ -0,0 +1,40 @@
+package br.com.ecommerce.accounts.integration;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
+
+import br.com.ecommerce.accounts.repository.UserRepository;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase
+class UserAdminCreationTest {
+
+    @Autowired
+    private UserRepository repository;
+    @Value("${user.admin.username}")
+	private String username;
+    @Value("${user.admin.password}")
+	private String password;
+    @Autowired
+    private BCryptPasswordEncoder encoder;
+
+
+    @Test
+    void verifyIfUserAdminIsPersisted() {
+        // act
+        var admin = repository.findByUsername(username);
+        
+        // assert
+        assertNotNull(admin.get());
+        assertTrue(encoder.matches(password, admin.get().getPassword()));
+    }
+}
