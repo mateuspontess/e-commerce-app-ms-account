3fe8522dd45fdc1c6b3e185d1e985348cb5eda45
Mateus Pontes <mateuspsdd@gmail.com>
2024-06-10 22:31:56 -0300

    feat: add `@Builder` annotation to `User`

--- src/main/java/br/com/ecommerce/accounts/model/User.java
+++ src/main/java/br/com/ecommerce/accounts/model/User.java
@@ -10,9 +10,11 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
